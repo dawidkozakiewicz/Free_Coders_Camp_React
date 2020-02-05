b517d217a1daa98beeff23fed01e5785aa6e3759
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-05 19:33:42 +0100

    rendering

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const JSX = (
     <div>
-      <h1>This is a block of JSX</h1>
-      <p>Here's a subtitle</p>
-      {/*comment*/}
+        <h1>Hello World</h1>
+        <p>Lets render this to the DOM</p>
     </div>
-  );
-  
+);
+
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
