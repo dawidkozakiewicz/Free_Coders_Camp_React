ec59b7be7d2f0ff5afdb850c48f1f85d8d3d70fe
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-09 19:56:46 +0100

    Compose React Components

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-const TypesOfFruit = () => {
+const NonCitrus = () => {
     return (
         <div>
-            <h2>Fruits:</h2>
+            <h3>Non Citrus:</h3>
             <ul>
                 <li>Apples</li>
                 <li>Blueberries</li>
                 <li>Strawberries</li>
                 <li>Bananas</li>
-            </ul>
-        </div>
-    );
-};
+            </ul></div>
+    )
+}
 
-const Fruits = () => {
+const Citrus = () => {
     return (
         <div>
-            <TypesOfFruit />
+            <h3>Citrus:</h3>
+            <ul>
+                <li>Lemon</li>
+                <li>Lime</li>
+                <li>Grapefruit</li>
+                <li>Orange</li>
+            </ul>
         </div>
-    );
+    )
+}
+
+const Vegetables = () => {
+    return (
+        <div>
+            <h2>Vegetables:</h2>
+            <ul>
+                <li>Brussel Sprouts</li>
+                <li>Broccoli</li>
+                <li>Squash</li>
+            </ul>
+        </div>
+    )
+}
+
+
+class Fruits extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2>Fruits:</h2>
+                <NonCitrus />
+                <Citrus />
+            </div>
+        );
+    }
 };
 
 class TypesOfFood extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <div>
                 <h1>Types of Food:</h1>
+                { /* change code below this line */}
                 <Fruits />
+                { /* change code above this line */}
+                <Vegetables />
             </div>
         );
     }
