5e895d04748854a90c2465644dbd955490bcadf4
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-10 22:56:56 +0100

    13_Pass_Props_to_a_Stateless_Functional_Component

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-class MyComponent extends React.Component {
+const CurrentDate = (props) => {
+    return (
+      <div>
+        <p>The current date is: {props.date}</p>
+      </div>
+    );
+  };
+   
+  class Calendar extends React.Component {
     constructor(props) {
-        super(props)
+      super(props);
     }
     render() {
-        return (
-            <div>
-            <h1>My First React Component!</h1>
-            </div>
-        )
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          <CurrentDate date={Date()} />
+        </div>
+      );
     }
-}
+  };
+  
  
-ReactDOM.render(<MyComponent/>, document.getElementById('challenge-node'))
+ReactDOM.render(<Calendar/>, document.getElementById('challenge-node'))
 
 
