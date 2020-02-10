e2b378378777db15f57af187cfcb259970a68329
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-10 22:28:40 +0100

    Write a React Component from Scratch

--- index.js
+++ index.js
@@ -1,75 +1,16 @@
-const NonCitrus = () => {
-    return (
-        <div>
-            <h3>Non Citrus:</h3>
-            <ul>
-                <li>Apples</li>
-                <li>Blueberries</li>
-                <li>Strawberries</li>
-                <li>Bananas</li>
-            </ul></div>
-    )
-}
-
-const Citrus = () => {
-    return (
-        <div>
-            <h3>Citrus:</h3>
-            <ul>
-                <li>Lemon</li>
-                <li>Lime</li>
-                <li>Grapefruit</li>
-                <li>Orange</li>
-            </ul>
-        </div>
-    )
-}
-
-const Vegetables = () => {
-    return (
-        <div>
-            <h2>Vegetables:</h2>
-            <ul>
-                <li>Brussel Sprouts</li>
-                <li>Broccoli</li>
-                <li>Squash</li>
-            </ul>
-        </div>
-    )
-}
-
-
-class Fruits extends React.Component {
+class MyComponent extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
     }
     render() {
         return (
             <div>
-                <h2>Fruits:</h2>
-                <NonCitrus />
-                <Citrus />
+            <h1>My First React Component!</h1>
             </div>
-        );
+        )
     }
-};
+}
+ 
+ReactDOM.render(<MyComponent/>, document.getElementById('challenge-node'))
 
-class TypesOfFood extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <h1>Types of Food:</h1>
-                <Fruits />
-                <Vegetables />
-            </div>
-        );
-    }
-};
 
-ReactDOM.render(
-    <TypesOfFood />,
-    document.getElementById('challenge-node')
-);
