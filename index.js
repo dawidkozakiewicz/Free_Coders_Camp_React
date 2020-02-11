4f1200e1ba007c7beb994e6933595e3d6c2234a7
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-11 22:52:51 +0100

    override default props

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const ShoppingCart = (props) => {
-  return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-      <p>Items: {props.items}</p>
-    </div>
-  )
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items quantity={10} />
+  }
 };
-ShoppingCart.defaultProps = { items: 0 }
+
 
 
 
