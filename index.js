78e8c52cc3522514ab8801d521eba746bbf5235f
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-18 23:23:55 +0100

    Pass a Callback as PropsPassed

--- index.js
+++ index.js
@@ -2,27 +2,54 @@ class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'CamperBot'
+      inputValue: ''
     }
+  this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
   }
   render() {
     return (
        <div>
-         <Navbar name={this.state.name}/>
+         <GetInput
+           input={this.state.inputValue}
+           handleChange={this.handleChange}/>
+         <RenderInput
+           input={this.state.inputValue}/>
        </div>
     );
   }
 };
 
-class Navbar extends React.Component {
+class GetInput extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-    <div>
-      <h1>Hello, my name is: {this.props.name} </h1>
-    </div>
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
     );
   }
 };
