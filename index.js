f71d2696b85fa11e8139968212130bea68866f8e
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-20 23:33:52 +0100

    add component will mount

--- index.js
+++ index.js
@@ -1,57 +1,13 @@
-class MyApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    }
-  this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      inputValue: event.target.value
-    });
-  }
-  render() {
-    return (
-       <div>
-         <GetInput
-           input={this.state.inputValue}
-           handleChange={this.handleChange}/>
-         <RenderInput
-           input={this.state.inputValue}/>
-       </div>
-    );
-  }
-};
-
-class GetInput extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
   }
-  render() {
-    return (
-      <div>
-        <h3>Get Input:</h3>
-        <input
-          value={this.props.input}
-          onChange={this.props.handleChange}/>
-      </div>
-    );
-  }
-};
-
-class RenderInput extends React.Component {
-  constructor(props) {
-    super(props);
+  componentWillMount() {
+    console.log("component ");
   }
   render() {
-    return (
-      <div>
-        <h3>Input Render:</h3>
-        <p>{this.props.input}</p>
-      </div>
-    );
+    return <div />;
   }
-};
+}
 
-ReactDOM.render(<MyApp />, document.getElementById("challenge-node"));
+ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
