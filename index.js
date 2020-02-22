f79a35afdd5c72dc6636f311f2688dc43074ef3d
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-22 23:17:28 +0100

    js changes

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUsers: null
+      message: ""
     };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   componentDidMount() {
-    setTimeout( () => {
-      this.setState({
-        activeUsers: 1273
-      });
-    }, 2500);
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+  handleEnter() {
+    this.setState({
+      message: this.state.message + "You pressed the enter key! "
+    });
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
   }
   render() {
     return (
       <div>
-        <h1>Active Users: {this.state.activeUsers}</h1>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
-};
+}
 
 
 ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
