7f2fb04a1d52e4a0ce525d70d866ac0a80531cc6
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-26 21:04:50 +0100

    Use a Ternary Expression for Conditional RenderingPassed

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-class MyComponent extends React.Component {
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: true
+      userAge: "",
+      input: ""
     };
-    this.toggleDisplay = this.toggleDisplay.bind(this);
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  toggleDisplay() {
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ""
+    });
+  }
+  submit() {
     this.setState(state => ({
-      display: !state.display
+      userAge: state.input
     }));
   }
   render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
     return (
       <div>
-        <button onClick={this.toggleDisplay}>Toggle Display</button>
-        {this.state.display && <h1>Displayed!</h1>}
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {this.state.userAge === ""
+          ? buttonOne
+          : this.state.userAge >= 18
+          ? buttonTwo
+          : buttonThree}
       </div>
     );
   }
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
+ReactDOM.render(<CheckUserAge />, document.getElementById("challenge-node"));
