2214b210c8f86f02258b81f788f44b5470ad5226
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-02-28 22:43:16 +0100

    Change Inline CSS Conditionally Based on Component State

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-class Results extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <h1>{this.props.fiftyFifty ? "You Win!" : "You Lose!"}</h1>;
-  }
-}
-
-class GameOfChance extends React.Component {
+class GateKeeper extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 1
+      input: ''
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleClick() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
+  handleChange(event) {
+    this.setState({ input: event.target.value })
   }
   render() {
-    const expression = Math.random() >= 0.5;
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // change code below this line
+this.state.input.length > 15 ? inputStyle ={border: '3px solid red'} : inputStyle = {
+      border: '1px solid black'
+    }
+    // change code above this line
     return (
       <div>
-        <button onClick={this.handleClick}>Play Again</button>
-        <Results fiftyFifty={expression} />
-        <p>{"Turn: " + this.state.counter}</p>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
       </div>
     );
   }
-}
+};
 
-ReactDOM.render(<GameOfChance />, document.getElementById("challenge-node"));
+
+ReactDOM.render(<GateKeeper />, document.getElementById("challenge-node"));
